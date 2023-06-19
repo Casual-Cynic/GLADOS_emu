63c8c2fe395a6cb495eb015c63b5fa82d0411bc7
jacobgelliss <jacobgelliss@gmail.com>
2023-06-19 05:23:29 -0500

    updated ascii art

--- apature_ext.py
+++ apature_ext.py
@@ -1,14 +1,17 @@
 from pydub import AudioSegment
 from pydub.playback import play as ps
+from colorama import init as colorama_init
+from colorama import Fore, Style
 
 class ApatureExtensions:
     def stillAlive():
         still_alive_wav = AudioSegment.from_wav('assets/wav/still_alive_8b.wav')
 
+        print(f"\n{Fore.GREEN}=====================================================================================\n")
         with open('assets/ascii/apature_logo.txt') as f:
             print(f.read())
+        print(f"\n====================================================================================={Style.RESET_ALL}")
         ps(still_alive_wav)
-        print("playing song")
 
 def main():
     ApatureExtensions.stillAlive()
