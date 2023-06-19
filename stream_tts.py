fedf986a32a407c01b5bbbf40c39233cce3c820c
jacobgelliss <jacobgelliss@gmail.com>
2023-06-19 02:15:34 -0500

    Update stream_tts.py

--- stream_tts.py
+++ stream_tts.py
@@ -1,9 +1,12 @@
 import sys
 from elevenlabs import set_api_key, generate, play
+from decouple import config
+
+TTS_KEY = config("TTS_KEY")
 
 audio = generate(
   text=str(sys.argv[1]),
-  api_key="cd1d02e15c60d923d70581f3b6eda3c7",
+  api_key=TTS_KEY,
   voice="SX4RvpkwlzfN6wip0Ejh",
   model="eleven_monolingual_v1"
 )
